e017633d304756742f3035e17cc3d190100c3409
sorett <sorett@nsdi.com.tw>
2020-03-03 15:18:30 +0800

    Initial Commit

---
+++ travelStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travelStory
+//
+//  Created by sorett on 2020/3/3.
+//  Copyright © 2020 sorett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
